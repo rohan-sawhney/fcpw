218f58ace84ce0ef1dd0fed579a82f2a8e14e5e2
rohan-sawhney <sawhney_rohan@yahoo.co.in>
2020-07-27 12:43:51 -0400

    moved to core

--- include/fcpw/core/core.h
+++ include/fcpw/core/core.h
@@ -12,9 +12,36 @@
 #include <chrono>
 #include <random>
 #include <type_traits>
-#include <fcpw/core/vector_operations.h>
+#ifdef FCPW_USE_ENOKI
+	#include <enoki/array.h>
+#endif
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 
 namespace fcpw {
+
+#ifdef FCPW_USE_ENOKI
+	template<size_t WIDTH>
+	using IntP = enoki::Packet<int, WIDTH>;
+	template<size_t WIDTH>
+	using FloatP = enoki::Packet<float, WIDTH>;
+	template<size_t WIDTH>
+	using MaskP = enoki::mask_t<FloatP<WIDTH>>;
+	template<size_t WIDTH, size_t DIM>
+	using VectorP = enoki::Array<FloatP<WIDTH>, DIM>;
+	template<size_t WIDTH>
+	using Vector2P = VectorP<WIDTH, 2>;
+	template<size_t WIDTH>
+	using Vector3P = VectorP<WIDTH, 3>;
+#endif
+
+template<size_t DIM>
+using Vector = Eigen::Matrix<float, DIM, 1>;
+using Vector2 = Vector<2>;
+using Vector3 = Vector<3>;
+
+template<size_t DIM>
+using Transform = Eigen::Transform<float, DIM, Eigen::Affine>;
 
 template<size_t DIM>
 struct Ray;
